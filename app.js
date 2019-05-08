51091885401f19774fe8b4bf4022a21e3b272d48
Nick Manning <nickmanning214@gmail.com>
2019-05-08 19:07:32 -0400

    Add Paragraph UI Button

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
 
 window.IncCKEditorFeatures = {
     //The editor
     InlineEditor,
     //block
-    Paragraph
+    Paragraph,
+    //UI
+    ParagraphButtonUI
 }
