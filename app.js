58b0f93330842576af2070bb19492a429cf6a9a8
Nick Manning <nickmanning214@gmail.com>
2019-05-08 18:43:05 -0400

    stable af

---
+++ app.js
@@ -0,0 +1,9 @@
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+window.IncCKEditorFeatures = {
+    //The editor
+    InlineEditor,
+    //block
+    Paragraph
+}
