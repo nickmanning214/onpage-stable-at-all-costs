19c3391e1b4ea542bf1334f5513d0db81b318492
Nick Manning <nickmanning214@gmail.com>
2019-05-08 23:46:32 -0400

    Add heading

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+//The editor
 import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+
+//containers
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+//UI
+import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
 import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
 
 window.IncCKEditorFeatures = {
     //The editor
     InlineEditor,
-    //block
+    //containers
     Paragraph,
+    Heading,
     //UI
+    HeadingButtonsUI,
     ParagraphButtonUI
+    
 }
