96f02b38009811aebf28f2b37149d14d9dbc40f4
Nick Manning <nickmanning214@gmail.com>
2019-05-09 00:13:10 -0400

    essentials

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //The editor
 import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 
+//functionalities
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import Enter from '@ckeditor/ckeditor5-enter/src/enter';
+import ShiftEnter from '@ckeditor/ckeditor5-enter/src/shiftenter';
+import Typing from '@ckeditor/ckeditor5-typing/src/typing';
+import Undo from '@ckeditor/ckeditor5-undo/src/undo';
+
 //containers
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
 
 //UI
 import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
@@ -12,6 +20,12 @@ import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbutton
 window.IncCKEditorFeatures = {
     //The editor
     InlineEditor,
+    //functionalities
+    Clipboard,
+    Enter,
+    ShiftEnter,
+    Typing,
+    Undo,
     //containers
     Paragraph,
     Heading,
