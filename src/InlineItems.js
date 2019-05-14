e5cffcb0c6081ced9c89be78a2a0694201e6e0da
Nick Manning <nickmanning214@gmail.com>
2019-05-14 07:34:19 -0400

    add button

--- src/InlineItems.js
+++ src/InlineItems.js
@@ -1,4 +1,6 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
 import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 
@@ -63,12 +65,47 @@ class InlineItems extends Plugin {
         return this;
     }
 
+    addIcon2(){
+
+        const editor = this.editor;
+        const t = editor.t;
+
+        // The "simpleBox" button must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add( 'inlineitem', locale => {
+            // The state of the button will be bound to the widget command.
+            //const command = editor.commands.get( 'insertSimpleBox' );
+
+            // The button will be an instance of ButtonView.
+            const buttonView = new ButtonView( locale );
+
+            buttonView.set( {
+                // The t() function helps localize the editor. All strings enclosed in t() can be
+                // translated and change when the language of the editor changes.
+                label: t( 'Inline Item' ),
+                withText: true,
+                tooltip: true
+            } );
+
+            // Bind the state of the button to the command.
+            //buttonView.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
+
+            // Execute the command when the button is clicked (executed).
+            this.listenTo( buttonView, 'execute', editor.config.get('onButtonPress').callback
+                //editor.execute( 'insertSimpleBox' ) 
+            );
+
+            return buttonView;
+        } );
+    }
     init() {
         
         this.registerSchema();
         //this.elementToElement();
         this.upcast();
-        
+        this.addIcon2();
+       
+
 
     }
 }
